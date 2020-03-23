e040e5aac3739e7ad1aef12a9413395dd1fb8694
Jeff Long <banterability@users.noreply.github.com>
2020-03-23 11:07:31 -0500

    Update prettier; reformat

--- __tests__/index_test.js
+++ __tests__/index_test.js
@@ -2,7 +2,7 @@ var blueshift = require("../index");
 var knownColors = require("./known_values.json");
 
 describe("blueshift", () => {
-  Object.keys(knownColors).forEach(city => {
+  Object.keys(knownColors).forEach((city) => {
     test(`generates the expected color for ${city}`, () => {
       expect(blueshift(city)).toEqual(knownColors[city]);
     });
