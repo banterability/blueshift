3b27639bbd30801c37de4e8e7c7320d8ed80a061
Jeff Long <banterability@users.noreply.github.com>
2022-05-02 11:10:10 -0500

    Switch from `substr` to `substring`

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var md5 = require("md5");
 
-const blueshift = (cityName) => `#${md5(cityName).substr(0, 6)}`;
+const blueshift = (cityName) => `#${md5(cityName).substring(0, 6)}`;
 
 module.exports = blueshift;
