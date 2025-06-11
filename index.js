53cd1c3b7a5b1144b669697208a8201a9087e3f8
Jeff Long <banterability@users.noreply.github.com>
2025-06-10 23:30:36 -0500

    Prettier

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 
-const blueshift = (cityName) => `#${crypto.createHash('md5').update(cityName).digest('hex').substring(0, 6)}`;
+const blueshift = (cityName) =>
+  `#${crypto.createHash("md5").update(cityName).digest("hex").substring(0, 6)}`;
 
 module.exports = blueshift;
